9586584bfb203b8482ac8330605711424e390905
rishard <info@masterpro.ws>
2022-11-17 07:09:57 +0300

    tty-promt added

--- bin/start.rb
+++ bin/start.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
-require_relative '../lib/analytics.rb'
+require_relative '../lib/analytics'
 require 'dotenv/load'
 require 'typhoeus'
 require 'json'
 require 'terminal-table'
+require 'tty-prompt'
+require 'date'
+
+prompt = TTY::Prompt.new
 
 puts 'Enter time period'
-puts 'Start date: (2020-01-29 for example)'
-date1 = gets.strip
-puts 'Final date: (2020-01-29 for example)'
-date2 = gets.strip
+# puts 'Start date: (2020-01-29 for example)'
+# date1 = gets.strip
+# puts 'Final date: (2020-01-29 for example)'
+# date2 = gets.strip
+date1 = prompt.ask('Start date:', default: DateTime.now.strftime('%Y-%m-01'))
+date2 = prompt.ask('Final date:', default: DateTime.now.strftime('%Y-%m-%d'))
 
 Analytics.table(date1, date2)
--- lib/analytics.rb
+++ lib/analytics.rb
@@ -1,16 +1,27 @@
 # frozen_string_literal: true
 
-require_relative './yandex_metrika.rb'
+require_relative './yandex_metrika'
 class Analytics
   def self.table(date1, date2)
     rows = []
-    title = 'Yandex Advertising Network Statistic (' + date1 + ':' + date2 + ')'
+    title = 'Yandex Advertising Network Statistic ('
+    + date1
+    + ':'
+    + date2
+    + ')'
     headings = ['URL (last 100 characters)', 'VISITS', 'PRICE']
     obj = query(date1, date2)
     obj['data'].each do |i|
-      rows << [i['dimensions'][0]['name'].split(//).last(100).join, i['metrics'][0].to_i, i['metrics'][2].round(2)]
+      rows << [i['dimensions'][0]['name']
+              .split(//)
+              .last(100)
+              .join,
+               i['metrics'][0].to_i,
+               i['metrics'][2].round(2)]
     end
-    puts Terminal::Table.new title: title, headings: headings, rows: rows
+    puts Terminal::Table.new title: title,
+                             headings: headings,
+                             rows: rows
     puts 'Total: ' + obj['totals'][2].round(2).to_s
   end
 
